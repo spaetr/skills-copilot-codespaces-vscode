47bac6f9918852de9fd06a3319acbfa3bfd2bf9d
spaetr <113061087+spaetr@users.noreply.github.com>
2023-07-21 19:03:30 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    return {
+        name: 'skillsMember',
+        restrict: 'E',
+        templateUrl: 'app/components/member/skills-member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'skillsMemberCtrl'
+    };
+}
